d24ddee0ac3784681e3ad4707e934e2dc054ffb8
Prince Neres <prince84224960@gmail.com>
2023-06-09 12:43:07 -0300

    Portlet MVC liferay para adicionar usuários com campos customizáveis

---
+++ src/main/java/user/register/constants/UserRegisterPortletKeys.java
@@ -0,0 +1,11 @@
+package user.register.constants;
+
+/**
+ * @author prince
+ */
+public class UserRegisterPortletKeys {
+
+	public static final String USERREGISTER =
+		"user_register_UserRegisterPortlet";
+
+}
